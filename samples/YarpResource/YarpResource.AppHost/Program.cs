4cc2453605934895033c187755e10625d1cc0d47
Damian Edwards <damian@damianedwards.com>
2024-05-23 20:37:37 -0700

    Add Yarp sample (broken)

---
+++ samples/YarpResource/YarpResource.AppHost/Program.cs
@@ -0,0 +1,12 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var app1 = builder.AddProject<Projects.WebApplication1>("app1");
+var app2 = builder.AddProject<Projects.WebApplication1>("app2");
+
+builder.AddYarp("ingress")
+    .WithHttpEndpoint(port: 8001)
+    .WithReference(app1)
+    .WithReference(app2)
+    .LoadFromConfiguration("ReverseProxy");
+
+builder.Build().Run();
