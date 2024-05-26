31ee7baf163ff7d3c8b88fd336e6fa3154e9b886
Damian Edwards <damian@damianedwards.com>
2024-05-26 13:24:03 -0700

    Add some unit tests

---
+++ tests/Aspirant.Hosting.UnitTests/RunWithTests.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics.CodeAnalysis;
+using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aspirant.Hosting.UnitTests;
+
+public class RunWithTests
+{
+    [Fact]
+    public void SimpleEnvironmentWithNameAndValueAddedWhenRunning()
+    {
+        var builder = DistributedApplication.CreateBuilder();
+        var resource = builder.AddResource(new ResourceA("resourceA"))
+            .WithEnvironment("first", "firstValue")
+            .RunWithEnvironment("myName", "value");
+
+        using var app = builder.Build();
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+        var resourceA = appModel.Resources.First(p => p.Name == "resourceA");
+
+        var environmentVariables = MaterializeEnvironmentVariables(app, resourceA);
+        Assert.Equal("value", environmentVariables["myName"]);
+    }
+
+    [Fact]
+    public void SimpleEnvironmentWithNameAndValueNotAddedWhenPublishing()
+    {
+        var builder = DistributedApplication.CreateBuilder(["Publishing:Publisher=manifest"]);
+        var resource = builder.AddResource(new ResourceA("resourceA"))
+            .WithEnvironment("first", "firstValue")
+            .RunWithEnvironment("myName", "value");
+
+        using var app = builder.Build();
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+        var resourceA = appModel.Resources.First(p => p.Name == "resourceA");
+
+        var environmentVariables = MaterializeEnvironmentVariables(app, resourceA);
+        Assert.DoesNotContain("myName", environmentVariables.Keys);
+    }
+
+    private static Dictionary<string, object> MaterializeEnvironmentVariables(DistributedApplication app, IResource resource)
+    {
+        Dictionary<string, object> environmentVariables = [];
+
+        if (resource.TryGetEnvironmentVariables(out var annotations))
+        {
+            var executionContext = app.Services.GetRequiredService<DistributedApplicationExecutionContext>();
+            var context = new EnvironmentCallbackContext(executionContext, environmentVariables);
+            foreach (var callbackAnnotation in annotations)
+            {
+                callbackAnnotation.Callback(context);
+            }
+        }
+
+        return environmentVariables;
+    }
+
+    private sealed class ResourceA(string name) : IResourceWithEnvironment
+    {
+        public string Name => name;
+
+        public ResourceAnnotationCollection Annotations { get; } = [];
+    }
+}
