a8e5337c00e9111aa3a5d3077822c41306c77862
Damian Edwards <damian@damianedwards.com>
2024-06-02 16:17:18 -0700

    Add more RunWith/PublishWith extension methods (#9)
    * Add RunWith/PublishWith methods for IResourceBuilder<ContainerResource>
    
    * MORE EXTENSION METHODS
    
    * Some more extension methods
    
    * Add Aspirant.Hosting.Redis

---
+++ src/Aspirant.Hosting.Redis/RedisResourceBuilderExtensions.PublishWith.cs
@@ -0,0 +1,109 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Redis;
+
+namespace Aspirant.Hosting;
+
+public static partial class RedisResourceBuilderExtensions
+{
+    /// <summary>
+    /// Configures the host port that the Redis Commander resource is exposed on instead of using randomly assigned port when
+    /// <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>..
+    /// </summary>
+    /// <param name="builder">The resource builder for Redis Commander.</param>
+    /// <param name="port">The port to bind on the host. If <see langword="null"/> is used random port will be assigned.</param>
+    /// <returns>The resource builder for PGAdmin.</returns>
+    public static IResourceBuilder<RedisCommanderResource> PublishWithHostPort(this IResourceBuilder<RedisCommanderResource> builder, int? port)
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithHostPort(port);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a named volume for the data folder to a Redis container resource and enables Redis persistence when
+    /// <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <remarks>
+    /// Use <see cref="PublishWithPersistence(IResourceBuilder{RedisResource}, TimeSpan?, long)"/> to adjust Redis persistence configuration, e.g.:
+    /// <code>
+    /// var cache = builder.AddRedis("cache")
+    ///                    .PublishWithDataVolume()
+    ///                    .PublishWithPersistence(TimeSpan.FromSeconds(10), 5);
+    /// </code>
+    /// </remarks>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="name">The name of the volume. Defaults to an auto-generated name based on the application and resource names.</param>
+    /// <param name="isReadOnly">
+    /// A flag that indicates if this is a read-only volume. Setting this to <c>true</c> will disable Redis persistence.<br/>
+    /// Defaults to <c>false</c>.
+    /// </param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<RedisResource> PublishWithDataVolume(this IResourceBuilder<RedisResource> builder, string? name = null, bool isReadOnly = false)
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithDataVolume(name, isReadOnly);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a bind mount for the data folder to a Redis container resource and enables Redis persistence
+    /// when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <remarks>
+    /// Use <see cref="PublishWithPersistence(IResourceBuilder{RedisResource}, TimeSpan?, long)"/> to adjust Redis persistence configuration, e.g.:
+    /// <code>
+    /// var cache = builder.AddRedis("cache")
+    ///                    .PublishWithDataBindMount()
+    ///                    .PublishWithPersistence(TimeSpan.FromSeconds(10), 5);
+    /// </code>
+    /// </remarks>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="source">The source directory on the host to mount into the container.</param>
+    /// <param name="isReadOnly">
+    /// A flag that indicates if this is a read-only mount. Setting this to <c>true</c> will disable Redis persistence.<br/>
+    /// Defaults to <c>false</c>.
+    /// </param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<RedisResource> PublishWithDataBindMount(this IResourceBuilder<RedisResource> builder, string source, bool isReadOnly = false)
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithDataBindMount(source, isReadOnly);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures a Redis container resource for persistence when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <remarks>
+    /// Use with <see cref="PublishWithDataBindMount(IResourceBuilder{RedisResource}, string, bool)"/>
+    /// or <see cref="PublishWithDataVolume(IResourceBuilder{RedisResource}, string?, bool)"/> to persist Redis data across sessions with custom persistence configuration, e.g.:
+    /// <code>
+    /// var cache = builder.AddRedis("cache")
+    ///                    .WithDataVolume()
+    ///                    .WithPersistence(TimeSpan.FromSeconds(10), 5);
+    /// </code>
+    /// </remarks>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="interval">The interval between snapshot exports. Defaults to 60 seconds.</param>
+    /// <param name="keysChangedThreshold">The number of key change operations required to trigger a snapshot at the interval. Defaults to 1.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<RedisResource> PublishWithPersistence(this IResourceBuilder<RedisResource> builder, TimeSpan? interval = null, long keysChangedThreshold = 1)
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithPersistence(interval, keysChangedThreshold);
+        }
+
+        return builder;
+    }
+}
---
+++ src/Aspirant.Hosting/ContainerResourceBuilderExtensions.RunWith.cs
@@ -0,0 +1,203 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting;
+
+namespace Aspirant.Hosting;
+
+public static partial class ContainerResourceBuilderExtensions
+{
+    /// <summary>
+    /// Adds the arguments to be passed to a container resource when the container is started when
+    /// <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="args">The arguments to be passed to the container when it is started.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithArgs<T>(this IResourceBuilder<T> builder, params string[] args) where T : IResourceWithArgs
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithArgs(args);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a callback to be executed with a list of command-line arguments when a container resource is started when
+    /// <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="callback">A callback that allows for deferred execution for computing arguments. This runs after resources have been allocated by the orchestrator and allows access to other resources to resolve computed data, e.g. connection strings, ports.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithArgs<T>(this IResourceBuilder<T> builder, Action<CommandLineArgsCallbackContext> callback) where T : IResourceWithArgs
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithArgs(callback);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a callback to be executed with a list of command-line arguments when a container resource is started when
+    /// <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="callback">A callback that allows for deferred execution for computing arguments. This runs after resources have been allocated by the orchestrator and allows access to other resources to resolve computed data, e.g. connection strings, ports.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithArgs<T>(this IResourceBuilder<T> builder, Func<CommandLineArgsCallbackContext, Task> callback) where T : IResourceWithArgs
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithArgs(callback);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a bind mount to a container resource when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="TResource"></typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="source">The source path of the mount. This is the path to the file or directory on the host.</param>
+    /// <param name="target">The target path where the file or directory is mounted in the container.</param>
+    /// <param name="isReadOnly">A flag that indicates if this is a read-only mount.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<TResource> RunWithBindMount<TResource>(this IResourceBuilder<TResource> builder, string source, string target, bool isReadOnly = false)
+        where TResource : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithBindMount(source, target, isReadOnly);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a volume to a container resource when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="name">The name of the volume.</param>
+    /// <param name="target">The target path where the volume is mounted in the container.</param>
+    /// <param name="isReadOnly">A flag that indicates if the volume should be mounted as read-only.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithVolume<T>(this IResourceBuilder<T> builder, string name, string target, bool isReadOnly = false) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithVolume(name, target, isReadOnly);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds an anonymous volume to a container resource when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="target">The target path where the volume is mounted in the container.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithVolume<T>(this IResourceBuilder<T> builder, string target) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithVolume(target);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Sets the Entrypoint for the container when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="entrypoint">The new entrypoint for the container.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithEntrypoint<T>(this IResourceBuilder<T> builder, string entrypoint) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithEntrypoint(entrypoint);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows overriding the image tag on a container when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="tag">Tag value.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithImageTag<T>(this IResourceBuilder<T> builder, string tag) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithImageTag(tag);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows overriding the image registry on a container when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="registry">Registry value.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> RunWithImageRegistry<T>(this IResourceBuilder<T> builder, string registry) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithImageRegistry(registry);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows overriding the image on a container when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="image">Image value.</param>
+    /// <param name="tag">Tag value.</param>
+    /// <returns></returns>
+    public static IResourceBuilder<T> RunWithImage<T>(this IResourceBuilder<T> builder, string image, string tag = "latest") where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithImage(image, tag);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows setting the image to a specific sha256 on a container when <see cref="DistributedApplicationExecutionContext.IsRunMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="sha256">Registry value.</param>
+    /// <returns></returns>
+    public static IResourceBuilder<T>RunWithImageSHA256<T>(this IResourceBuilder<T> builder, string sha256) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode)
+        {
+            builder.WithImageSHA256(sha256);
+        }
+
+        return builder;
+    }
+}
