7e32045077d68be94932a8699d60209a8f731528
Damian Edwards <damian@damianedwards.com>
2024-06-02 15:37:32 -0700

    Add RunWith/PublishWith methods for IResourceBuilder<ContainerResource>

---
+++ src/Aspirant.Hosting/ContainerResourceBuilderExtensions.PublishWith.cs
@@ -0,0 +1,152 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting;
+
+namespace Aspirant.Hosting;
+
+/// <summary>
+/// Extension methods for <see cref="IResourceBuilder{ContainerResource}"/>.
+/// </summary>
+public static partial class ContainerResourceBuilderExtensions
+{
+    /// <summary>
+    /// Adds a bind mount to a container resource when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="TResource"></typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="source">The source path of the mount. This is the path to the file or directory on the host.</param>
+    /// <param name="target">The target path where the file or directory is mounted in the container.</param>
+    /// <param name="isReadOnly">A flag that indicates if this is a read-only mount.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<TResource> PublishWithBindMount<TResource>(this IResourceBuilder<TResource> builder, string source, string target, bool isReadOnly = false)
+        where TResource : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithBindMount(source, target, isReadOnly);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a volume to a container resource when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="name">The name of the volume.</param>
+    /// <param name="target">The target path where the volume is mounted in the container.</param>
+    /// <param name="isReadOnly">A flag that indicates if the volume should be mounted as read-only.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> PublishWithVolume<T>(this IResourceBuilder<T> builder, string name, string target, bool isReadOnly = false) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithVolume(name, target, isReadOnly);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds an anonymous volume to a container resource when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="target">The target path where the volume is mounted in the container.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> PublishWithVolume<T>(this IResourceBuilder<T> builder, string target) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithVolume(target);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Sets the Entrypoint for the container when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="entrypoint">The new entrypoint for the container.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> PublishWithEntrypoint<T>(this IResourceBuilder<T> builder, string entrypoint) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithEntrypoint(entrypoint);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows overriding the image tag on a container when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="tag">Tag value.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> PublishWithImageTag<T>(this IResourceBuilder<T> builder, string tag) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithImageTag(tag);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows overriding the image registry on a container when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="registry">Registry value.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<T> PublishWithImageRegistry<T>(this IResourceBuilder<T> builder, string registry) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithImageRegistry(registry);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows overriding the image on a container when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="image">Image value.</param>
+    /// <param name="tag">Tag value.</param>
+    /// <returns></returns>
+    public static IResourceBuilder<T> PublishWithImage<T>(this IResourceBuilder<T> builder, string image, string tag = "latest") where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithImage(image, tag);
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Allows setting the image to a specific sha256 on a container when <see cref="DistributedApplicationExecutionContext.IsPublishMode"/> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of container resource.</typeparam>
+    /// <param name="builder">Builder for the container resource.</param>
+    /// <param name="sha256">Registry value.</param>
+    /// <returns></returns>
+    public static IResourceBuilder<T>PublishWithImageSHA256<T>(this IResourceBuilder<T> builder, string sha256) where T : ContainerResource
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            builder.WithImageSHA256(sha256);
+        }
+
+        return builder;
+    }
+}
