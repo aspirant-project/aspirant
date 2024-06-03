a542884af5c57244300649faef26a09ad80169b3
Renato Golia <renato.golia@insightarchitectures.com>
2024-06-03 09:38:29 +0200

    Added Aspirant.Hosting.PostgreSQL

---
+++ src/Aspirant.Hosting.PostgreSQL/PostgreSqlHealthCheckExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using HealthChecks.NpgSql;
+
+namespace Aspirant.Hosting.PostgreSQL;
+
+/// <summary>
+/// Adds a health check to the PostgreSQL resources.
+/// </summary>
+public static class PostgreSqlHealthCheckExtensions
+{
+    /// <summary>
+    /// Adds a health check to the PostgreSQL server resource.
+    /// </summary>
+    public static IResourceBuilder<PostgresServerResource> WithHealthCheck(this IResourceBuilder<PostgresServerResource> builder)
+    {
+        return builder.WithAnnotation(HealthCheckAnnotation.Create(cs => new NpgSqlHealthCheck(new NpgSqlHealthCheckOptions(cs))));
+    }
+
+    /// <summary>
+    /// Adds a health check to the PostgreSQL database resource.
+    /// </summary>
+    public static IResourceBuilder<PostgresDatabaseResource> WithHealthCheck(this IResourceBuilder<PostgresDatabaseResource> builder)
+    {
+        return builder.WithAnnotation(HealthCheckAnnotation.Create(cs => new NpgSqlHealthCheck(new NpgSqlHealthCheckOptions(cs))));
+    }
+}
