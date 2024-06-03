c46c9b96ff3103f7831fc9a3722070d0c60fd715
Renato Golia <renato.golia@insightarchitectures.com>
2024-06-03 09:31:17 +0200

    Added Aspirant.Hosting.SqlServer

---
+++ src/Aspirant.Hosting.SqlServer/SqlResourceHealthCheckExtensions.cs
@@ -0,0 +1,48 @@
+using Aspire.Hosting.ApplicationModel;
+using HealthChecks.SqlServer;
+
+namespace Aspirant.Hosting;
+
+/// <summary>
+/// Adds a health check to the SQL Server server resource. 
+/// </summary>
+public static class SqlResourceHealthCheckExtensions
+{
+    /// <summary>
+    /// Adds a health check to the SQL Server server resource.
+    /// </summary>
+    public static IResourceBuilder<SqlServerServerResource> WithHealthCheck(this IResourceBuilder<SqlServerServerResource> builder)
+    {
+        return builder.WithSqlHealthCheck(cs => new SqlServerHealthCheckOptions { ConnectionString = cs });
+    }
+
+    /// <summary>
+    /// Adds a health check to the SQL Server database resource.
+    /// </summary>
+    public static IResourceBuilder<SqlServerDatabaseResource> WithHealthCheck(this IResourceBuilder<SqlServerDatabaseResource> builder)
+    {
+        return builder.WithSqlHealthCheck(cs => new SqlServerHealthCheckOptions { ConnectionString = cs });
+    }
+
+    /// <summary>
+    /// Adds a health check to the SQL Server server resource with a specific query.
+    /// </summary>
+    public static IResourceBuilder<SqlServerServerResource> WithHealthCheck(this IResourceBuilder<SqlServerServerResource> builder, string query)
+    {
+        return builder.WithSqlHealthCheck(cs => new SqlServerHealthCheckOptions { ConnectionString = cs, CommandText = query });
+    }
+
+    /// <summary>
+    /// Adds a health check to the SQL Server database resource  with a specific query.
+    /// </summary>
+    public static IResourceBuilder<SqlServerDatabaseResource> WithHealthCheck(this IResourceBuilder<SqlServerDatabaseResource> builder, string query)
+    {
+        return builder.WithSqlHealthCheck(cs => new SqlServerHealthCheckOptions { ConnectionString = cs, CommandText = query });
+    }
+
+    private static IResourceBuilder<T> WithSqlHealthCheck<T>(this IResourceBuilder<T> builder, Func<string, SqlServerHealthCheckOptions> healthCheckOptionsFactory)
+        where T: IResource
+    {
+        return builder.WithAnnotation(HealthCheckAnnotation.Create(cs => new SqlServerHealthCheck(healthCheckOptionsFactory(cs))));
+    }
+}
