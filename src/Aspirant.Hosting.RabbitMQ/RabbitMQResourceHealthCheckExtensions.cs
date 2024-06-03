245520ba273177d6495ca8cc774519da18fb988e
Renato Golia <renato@golia.tech>
2024-06-03 18:55:27 +0200

    Add resource-specific packages with support for health checks (#11)
    * Added Aspirant.Hosting.SqlServer
    
    * Added Aspirant.Hosting.PostgreSQL
    
    * Added healthchecks to Aspirant.Hosting.Redis
    
    * Added Aspirant.Hosting.RabbitMQ

---
+++ src/Aspirant.Hosting.RabbitMQ/RabbitMQResourceHealthCheckExtensions.cs
@@ -0,0 +1,18 @@
+﻿﻿using Aspire.Hosting.ApplicationModel;
+using HealthChecks.RabbitMQ;
+
+namespace Aspirant.Hosting.RabbitMQ;
+
+/// <summary>
+/// Adds a health check to RabbitMQ resources.
+/// </summary>
+public static class RabbitMQResourceHealthCheckExtensions
+{
+    /// <summary>
+    /// Adds a health check to the RabbitMQ server resource.
+    /// </summary>
+    public static IResourceBuilder<RabbitMQServerResource> WithHealthCheck(this IResourceBuilder<RabbitMQServerResource> builder)
+    {
+        return builder.WithAnnotation(HealthCheckAnnotation.Create(cs => new RabbitMQHealthCheck(new RabbitMQHealthCheckOptions { ConnectionUri = new(cs) })));
+    }
+}
---
+++ src/Aspirant.Hosting.Redis/RedisResourceHealthCheckExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using HealthChecks.Redis;
+
+namespace Aspirant.Hosting;
+
+/// <summary>
+/// Adds a health check to Redis resources.
+/// </summary>
+public static class RedisResourceHealthCheckExtensions
+{
+    /// <summary>
+    /// Adds a health check to the Redis server resource.
+    /// </summary>
+    public static IResourceBuilder<RedisResource> WithHealthCheck(this IResourceBuilder<RedisResource> builder)
+    {
+        return builder.WithAnnotation(HealthCheckAnnotation.Create(cs => new RedisHealthCheck(cs)));
+    }
+}
